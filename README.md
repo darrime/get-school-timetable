734be2766893d45486a3bcc3aba62bc9c4d145c1
darrime <darrime627@gmail.com>
2021-07-23 22:44:36 +0900

    최종 릴리즈(1.0.0)

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # get-school-timetable  
-컴시간알리미의 학교 시간표를 크롤링합니다.  
-selenium과 Chrome Webdriver를 필요로 합니다.  
+컴시간알리미 크롤링 라이브러리(Python)  
+selenium과 Chrome Webdriver, pathos(멀티프로세싱, 필수 아님)를 필요로 합니다.  
+  
+## 주의!! loading_timetable 함수 2번 이상 사용시 멀티프로세싱 사용 필수
+selenium webdriver가 single core라서 2번 이상 호출시 에러..  
+
   
 ## install selenium
 ```
 pip install selenium
+```
+## install pathos
+```
+pip install pathos
+```
+
+## 사용 예시
+### 파이썬 코드
+```
+from getTime import loading_timetable
+
+# 중학교 이름 정확히/학년-반 순으로
+sub_list = loading_timetable('퇴계원중학교', '3-6')
+
+# 배열이 아닌 딕셔너리(배열을 포함한 딕셔너리). 첫번째 key값은 '월', '화', '수' 등으로 입력.
+# 배열은 숫자 0부터.(1교시 -> 0, 2교시 -> 1)
+print(sub_list['월'][0])  # 월요일 1교시 과목은?
+
+print(sub_list)  # 전체 시간표(일주일) 출력, 형식은 딕셔너리
+```
+###출력값
+```
+#(print(sub_list['월'][0]) 
+미술
+print(sub_list)
+{'월': ['미술', '사회', '영어', '국어', '중국', '창체', ''],
+ '화': ['국어', '영어', '체육', '기가', '사회', '과학', '수학'],
+ '수': ['미술', '사회', '영어', '국어', '중국', '창체', ''], 
+ '목': ['중국', '영어', '수학', '국어', '과학', '역사', '음악'], 
+ '금': ['체육', '기가', '중국', '창체', '', '', '']}
 ```
--- getTime.py
+++ getTime.py
@@ -1,17 +1,11 @@
 import time
 
-from secret_var import *
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ex
 
-option = wd.ChromeOptions()
-option.add_argument("headless")
-
-browser = wd.Chrome(options=option)
-
-sub_list = {
+class_list = {
     '월': ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7'],
     '화': ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7'],
     '수': ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7'],
@@ -20,52 +14,43 @@ sub_list = {
 }
 
 
-def set_delay():
-    wait = WebDriverWait(browser, 10).until(
-        ex.presence_of_element_located((By.XPATH, '//*[@id="학교명단검색"]/tbody/tr[2]/td[1]')))
-    print(wait.text[0:0])
+def loading_timetable(school_name: str, class_number: str):
+    def set_delay():
+        wait = WebDriverWait(browser, 10).until(
+            ex.presence_of_element_located((By.XPATH, '//*[@id="학교명단검색"]/tbody/tr[2]/td[1]')))
+        print(wait.text[0:0])
 
+    def run_js_code(js_code):
+        browser.execute_script(js_code)
+        time.sleep(0.1)
 
-def run_js_code(js_code):
-    browser.execute_script(js_code)
-    time.sleep(0.1)
+    option = wd.ChromeOptions()
+    option.add_argument("headless")
 
+    browser = wd.Chrome(options=option)
 
-# 클래스 정의
-class getClassSubject:
-    # 이렇게 클래스 안에 넣어도 되는건지 모르겠다...
     browser.get("http://112.186.146.81:4082/st#")
-
-    elem = browser.find_element_by_id("sc")
-    button = browser.find_element_by_xpath('//*[@id="학교찾기"]/table[1]/tbody/tr[2]/td[2]/input[2]')
-
-    elem.click()
-    elem.send_keys(school_name)
-    button.click()
 
     # 효율적인 딜레이 구현
     try:
         set_delay()
-        print("-" * 6 + "로딩 성공" + "-" * 6)
     except:
         print("failed")
 
     # 자바스크립트 코드 목록
-    js_code = [school_code, 'bas = document.getElementById("ba")', f'bas.value = "{class_num}"', 'ba_change()']
+    js_code = [f'school_ra("{school_name}")',
+               'sc_disp(H학교명단.학교검색[0][3])',
+               'bas = document.getElementById("ba")',
+               f'bas.value = "{class_number}"',
+               'ba_change()']
 
     # 자바스크립트 코드 실행
     for i in js_code:
         run_js_code(i)
 
-    for k, i in enumerate(sub_list.keys(), 2):
-        for j in range(0, 6):
-            sub_list[i][j] = browser.find_element_by_xpath(
+    for k, i in enumerate(class_list.keys(), 2):
+        for j in range(0, 7):
+            class_list[i][j] = browser.find_element_by_xpath(
                 f'//*[@id="hour"]/table/tbody/tr[{j + 3}]/td[{k}]').text[0:2]
     browser.quit()
-
-    def __init__(self, day: str, class_time: int):
-        self.day = day
-        self.class_time = class_time
-
-    def getSub(self):
-        return sub_list[self.day][self.class_time - 1]
+    return class_list
