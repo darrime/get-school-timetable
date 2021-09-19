546e021e8e2e80f46787973f85d657232833250d
darrime <darrime627@gmail.com>
2021-09-19 17:33:11 +0900

    버전에 맞는 chromedriver 업데이트, 전체적인 코드 수정

--- getTime.py
+++ getTime.py
@@ -14,43 +14,50 @@ class_list = {
 }
 
 
-def loading_timetable(school_name: str, class_number: str):
-    def set_delay():
-        wait = WebDriverWait(browser, 10).until(
+class getSchoolTime:
+
+    def __init__(self, school_name: str, class_number: str):
+        self.school_name = school_name
+        self.class_number = class_number
+
+        option = wd.ChromeOptions()
+        option.add_argument("headless")
+
+        self.browser = wd.Chrome(options=option)
+
+    def set_delay(self):
+        wait = WebDriverWait(self.browser, 10).until(
             ex.presence_of_element_located((By.XPATH, '//*[@id="학교명단검색"]/tbody/tr[2]/td[1]')))
         print(wait.text[0:0])
 
-    def run_js_code(js_code):
-        browser.execute_script(js_code)
+    def run_js_code(self, js_code):
+        self.browser.execute_script(js_code)
         time.sleep(0.1)
 
-    option = wd.ChromeOptions()
-    option.add_argument("headless")
+    def loading_timetable(self):
 
-    browser = wd.Chrome(options=option)
+        self.browser.get("http://112.186.146.81:4082/st#")
 
-    browser.get("http://112.186.146.81:4082/st#")
+        # 효율적인 딜레이 구현
+        try:
+            self.set_delay()
+        except:
+            print("failed")
 
-    # 효율적인 딜레이 구현
-    try:
-        set_delay()
-    except:
-        print("failed")
+        # 자바스크립트 코드 목록
+        js_code = [f'school_ra("{self.school_name}")',
+                   'sc_disp(H학교명단.학교검색[0][3])',
+                   'bas = document.getElementById("ba")',
+                   f'bas.value = "{self.class_number}"',
+                   'ba_change()']
 
-    # 자바스크립트 코드 목록
-    js_code = [f'school_ra("{school_name}")',
-               'sc_disp(H학교명단.학교검색[0][3])',
-               'bas = document.getElementById("ba")',
-               f'bas.value = "{class_number}"',
-               'ba_change()']
+        # 자바스크립트 코드 실행
+        for i in js_code:
+            self.run_js_code(i)
 
-    # 자바스크립트 코드 실행
-    for i in js_code:
-        run_js_code(i)
-
-    for k, i in enumerate(class_list.keys(), 2):
-        for j in range(0, 7):
-            class_list[i][j] = browser.find_element_by_xpath(
-                f'//*[@id="hour"]/table/tbody/tr[{j + 3}]/td[{k}]').text[0:2]
-    browser.quit()
-    return class_list
+        for k, i in enumerate(class_list.keys(), 2):
+            for j in range(0, 7):
+                class_list[i][j] = self.browser.find_element_by_xpath(
+                    f'//*[@id="hour"]/table/tbody/tr[{j + 3}]/td[{k}]').text[0:2]
+        self.browser.quit()
+        return class_list
